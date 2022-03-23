b460a5a237738d1824d85d8929d3fcf249f4b8ce
Otong Sunandar <klapertartinc@gmail.com>
2022-03-23 16:41:41 +0700

    otong, enhance-1, menambahkan kolom email di halaman login

--- index.php
+++ index.php
@@ -79,6 +79,9 @@ span.psw {
     <label for="uname"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
 
+    <label for="uname"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
         
